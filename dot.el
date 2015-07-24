8d1b4054f178a7c6e6c73cf827270de263b56d34
Ivan Gotovchits <ivg@ieee.org>
2015-07-24 16:13:46 -0400

    added "dark-theme"

--- dot.el
+++ dot.el
@@ -1,8 +1,14 @@
-(defvar dot-command "dot -Tpng" 
+(defvar dot-command "dot -Tpng"
   "command to run on a dot representation")
 
+(defvar dot-darktheme nil
+  "reverse colors if set to t")
+
+(defvar dot-dark-node "node[color=gray;fontcolor=gray]")
+(defvar dot-dark-edge "edge[color=gray;fontcolor=gray]")
+
 (defun pread (start end command)
-  "runs COMMAND on a region specified by START and END and returns 
+  "runs COMMAND on a region specified by START and END and returns
    the result of command invokation as a string"
   (string-as-unibyte
    (with-output-to-string
@@ -18,11 +24,19 @@
   (let ((data (pread start end dot-command)))
     (dot-insert-image data)))
 
+(defun dot/night-colors (text)
+  (let* ((piv (1+ (string-match "{" text)))
+         (lhs (substring text 0 piv))
+         (rhs (substring text piv)))
+    (format "%s\nbgcolor=black\n%s\n%s\n%s"
+            lhs dot-dark-node dot-dark-edge rhs)))
+
 (defun dot-ocaml-toplevel-autoinsert (text)
   (let ((beg (string-match "digraph" text))
         (end (string-match "}" text)))
     (when (and beg end)
       (let* ((input (substring text beg (1+ end)))
+             (input (if dot-darktheme (dot/night-colors input) input))
              (command (format "echo '%s' | %s" input dot-command))
              (data (shell-command-to-string command))
              (pos (point)))
